4214f0600ec0da6e6121acb86032e1b61346fc86
audreythando <audreythando@gmail.com>
2022-11-09 17:29:12 +0200

    refactor: added firebase config

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCio2QtMMKWOEO1dog4zCel1sy2Z6IlZ4Q",
+  authDomain: "fir-eef7a.firebaseapp.com",
+  projectId: "fir-eef7a",
+  storageBucket: "fir-eef7a.appspot.com",
+  messagingSenderId: "90173679095",
+  appId: "1:90173679095:web:caaf3bc538be1892767af2"
+};
+
+const app = initializeApp(firebaseConfig);
